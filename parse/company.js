f70bf854b8f660f13a0dab46254082012e80d8b0
liuzhanliang <zhanliangliu@qq.com>
2018-08-18 21:11:18 +0800

    add nf.loc, nf.loc_province_city, nf.loc_city_county etc.

--- parse/company.js
+++ parse/company.js
@@ -1,6 +1,21 @@
 var nf = nf || {};
 
-nf.company = function(loc, name, type, area) {
-    return {"loc":loc, "name":name, "type":type, "area":area}
+nf.company = function(loc, name, type, scope) {
+    return {"loc":loc, "name":name, "type":type, "scope":scope};
 }
 
+nf.loc = function(l) {
+    return {"loc_code": l};
+}
+
+nf.loc_province_city = function(p, c) {
+    return {"province_code": p, "city_code": c};
+}
+
+nf.loc_province_county = function(p, county) {
+    return {"province_code": p, "county_code": c};
+}
+
+nf.loc_city_county = function(city, county) {
+    return {"city_code": city, "county_code": county};
+}
