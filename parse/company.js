1ec3b1c191b04365372cab045193666355165eb4
liuzhanliang <zhanliangliu@qq.com>
2018-08-20 09:07:10 +0800

    nf.list and nf.object

--- parse/company.js
+++ parse/company.js
@@ -1,5 +1,17 @@
 var nf = nf || {};
 
+nf.list = function() {
+    var arr = [];
+    for (var i = 0; i < arguments.length; i++) {
+        arr.push(arguments[i]);
+    }
+    return arr;
+}
+
+nf.object = function(type, value) {
+    return {"type": type, "value": value};
+}
+
 nf.company = function(loc, name, type, scope) {
     return {"loc":loc, "name":name, "type":type, "scope":scope};
 }
