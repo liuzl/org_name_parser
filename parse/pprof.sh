da0b7439578fccaea44b2945571cb3281eff99f5
liuzhanliang <zhanliangliu@qq.com>
2018-09-08 19:06:37 +0800

    batch run company.txt for pprof

--- parse/pprof.sh
+++ parse/pprof.sh
@@ -1,2 +1,2 @@
-./parse -dict_dir dicts -dict_name company -cpuprofile profile.prof > /dev/null
+./parse -dict_dir dicts -dict_name company -i company.txt -cpuprofile profile.prof > /dev/null
 go tool pprof ./parse profile.prof
